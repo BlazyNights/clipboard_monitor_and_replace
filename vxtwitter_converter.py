e6b1328bddd5cba77f94d4160c9c6bac337dae78
BlazyNights <BlazyNights@gmail.com>
2024-02-02 20:53:05 -0800

    It functions after being split into functions, reads rules from config files

--- vxtwitter_converter.py
+++ vxtwitter_converter.py
@@ -43,26 +43,80 @@ def main():
         pyperclip.copy(clipboard_contents)
 
 
-def read_config():
-    pass
+def read_config() -> dict:
+    with open('config.json', 'r') as f:
+        return json.load(f)
 
 
-def write_config():
-    pass
+def write_config() -> None:
+    with open('config.json', 'w') as f:
+        __config = {
+            'line_replace': [
+                {'find': '/twitter.com', 'replace': '/vxtwitter.com'},
+                {'find': 'x.com', 'replace': 'fixupx.com'},
+                {'find': 'www.reddit.com', 'replace': 'old.reddit.com'}
+            ],
+            'line_partial_strip': [
+                {'urls': ('twitter.com', 'x.com', 'cdn.discordapp.com'), 'characters_to_strip_after': '?'},
+                {'urls': ('amazon.com', 'amazon.ca'), 'characters_to_strip_after': '/ref'}]
+        }
+        json.dump(__config, f, indent=4)
 
 
-def replace_text() -> str:
-    pass
+def replace_text(__config: dict, __clipboard_input: str) -> str:
+    """Find/replace using the config as rules"""
+    changed_text = ''
+    for find_replace_rule in __config['line_replace']:
+        if find_replace_rule['find'] in __clipboard_input:
+            changed_text = __clipboard_input.replace(find_replace_rule['find'], find_replace_rule['replace'])
+            # Stop because the intent is only to match one rule
+            break
+    return changed_text or __clipboard_input
 
 
-def strip_partial_line() -> str:
-    pass
+def strip_partial_line(__config: dict, __clipboard_input: str) -> str:
+    changed_text = ''
+    for strip_partial_line_rule in __config['line_partial_strip']:
+        if any(x in __clipboard_input for x in strip_partial_line_rule['urls']) \
+                and strip_partial_line_rule['characters_to_strip_after'] in __clipboard_input:
+            __clipboard_input = __clipboard_input.rsplit('/ref')[0]
+    return changed_text or __clipboard_input
+
+
+def clipboard_scan_and_replace(__config):
+    clipboard_contents = pyperclip.paste()
+    # print(clipboard_contents)
+    modified_clipboard = clipboard_contents
+
+    modified_clipboard = replace_text(config, modified_clipboard)
+    modified_clipboard = strip_partial_line(config, modified_clipboard)
+    # print(f'returned: {replace_text(config, modified_clipboard)}')
+
+    # Only update the clipboard if it's been changed
+    if clipboard_contents != modified_clipboard:
+        pyperclip.copy(modified_clipboard)
+        print(f'Modified: {modified_clipboard}')
 
 
 if __name__ == '__main__':
-    while True:
-        try:
-            main()
-            time.sleep(.5)
-        except pyperclip.PyperclipWindowsException as e:
-            pass
+    # old = True
+    old = False
+    if old:
+        while True:
+            try:
+                main()
+                time.sleep(.5)
+            except pyperclip.PyperclipWindowsException as e:
+                print(e)
+    else:
+        write_config()
+        config = read_config()
+
+        while True:
+            try:
+                clipboard_scan_and_replace(config)
+                time.sleep(.5)
+
+            except pyperclip.PyperclipWindowsException as e:
+                print(e)
+
